923978b3df66b3ff2725fa5e0afd0a822e2add02
grambo <gramham@live.com>
2020-04-13 15:26:19 -0600

    added a bunch of stuff

--- NBATEAM/color1.js
+++ NBATEAM/color1.js
@@ -1,22 +1,27 @@
 const teamlocations = require("./findpng.js");
 const path = require('path');
 const getColors = require('get-image-colors');
-let colorarray;
+let colorarray15 =[];
 let teamstring = require('./string.js');
-string.replace(/ /g, "");
+// string.replace(/ /g, "");
+let teamstring1 = teamlocations[teamstring];
 
-let currentteam = "/NBAlogos/" + teamlocations[teamstring] +".png";
+let currentteam = "/NBAlogos/" + teamstring1[0] +".png";
+let currentteamonlineurl = teamstring1[1];
 getColors(path.join(__dirname, currentteam)).then(colors => {
     
    colorarray = colors.map(color => color.hex());
    console.log(colorarray);
-   });
-
+  colorarray.forEach(element => {
+      colorarray15.push(element);
+  });
+});
+   
    let teamobj ={
-    url: currentteam,
-    colors: colorarray
-}
-module.exports = teamobj;
+       colors1: colorarray15,
+       url: currentteamonlineurl
+    }
+    module.exports = teamobj;
 
 
 
--- routes/userInfo-api-routes.js
+++ routes/userInfo-api-routes.js
@@ -1,6 +1,29 @@
 var db = require("../models");
 let handlebars = require("express-handlebars");
 var express = require('express');
+// var teamcolor = require('../NBATEAM/color1.js');
+const teamlocations = require("../NBATEAM/findpng.js");
+const path = require('path');
+const getColors = require('get-image-colors');
+let colorarray15 =[];
+// let teamstring = require('./string.js');
+let teamstring1 = teamlocations[teamstring];
+
+let currentteam = "/NBAlogos/" + teamstring1[0] +".png";
+let currentteamonlineurl = teamstring1[1];
+getColors(path.join(__dirname, currentteam)).then(colors => {
+    
+   colorarray = colors.map(color => color.hex());
+   console.log(colorarray);
+  colorarray.forEach(element => {
+      colorarray15.push(element);
+  });
+});
+   
+   let teamobj ={
+       colors1: colorarray15,
+       url: currentteamonlineurl
+    }
 //POST route for saving new user info
 module.exports = function(app) {
     app.post("/api/userInfo", function(req, res) {
@@ -34,6 +57,23 @@ module.exports = function(app) {
           };
           });
         });
+        app.get("/api/home/:id?", function(req, res){
 
-    
-}
+          db.UserInfo.findOne({
+            where: {
+              id: req.params.id
+            },
+          }).then(function(data) {
+         
+            let infoobject = {
+              db: data,
+              color: teamobj
+            }
+            console.log(infoobject);
+            // res.sendFile(teamcolor.url);
+            // res.sendFile(teamcolor.url);
+            res.send(infoobject);
+          
+          });
+        });
+};
